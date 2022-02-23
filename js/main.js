749290e5f247b22e184e5a60e9afb13d59fd8344
Vitalii Synchuk <vitaliisynchuk@gmail.com>
2022-02-23 09:09:23 +0100

    Добавил функций 3

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const getRandomIntInclusive = (min, max) => {
 
   if (min >= max) {
     [min, max] = [max, min];
-    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -13,6 +12,7 @@ getRandomIntInclusive();
 
 // Источик, https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-const checkStrokeLenght = (maxSymbolNumber, symbolNumber) => (symbolNumber <= maxSymbolNumber);
+const checkStrokeLength = (stroke, maxLength) => (stroke.length <= maxLength);
 
-checkStrokeLenght();
+checkStrokeLength();
+
