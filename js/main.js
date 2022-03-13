61f5e5d11eb59831b57cac1687411a96c6e7ca63
Vitalii Synchuk <vitaliisynchuk@gmail.com>
2022-03-13 16:56:54 +0100

    more detals 2

--- js/main.js
+++ js/main.js
@@ -10,63 +10,89 @@ const checkStrokeLength = (stroke, maxLength) => (stroke.length <= maxLength);
 checkStrokeLength(1, 13);
 
 function getRandomDiferentNumber (element, min, max) {
-  let random = (getRandomPositiveInteger (min, max));
+  let random = getRandomPositiveInteger(min, max);
   while (element.includes(random)) {
     random = (getRandomPositiveInteger (min, max));
   }
   element.push(random);
+
+  return random;
 }
 
 function getRandomArrayElement (element) {
-  return element[Math.floor(Math.random()*element.length)]
+  return element[Math.floor(Math.random()*element.length)];
 }
 
-let id = [];
-let url = [];
-let description = [
+const description = [
   'Красный утюг',
   'Голубое небо',
   'Желтое солнце',
   'Ярккий Фломастер',
 ];
-let likes = [];
-let commentId = [];
-let avatar = [];
-let message = [
+const message = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-]
-let name = [
+];
+const name = [
   'Павел',
   'Артур',
   'Анна',
   'Фиона',
   'Петро',
-]
+];
 
-getRandomDiferentNumber(id, 1, 25);
-getRandomDiferentNumber(url, 1, 25);
-getRandomDiferentNumber(likes, 15, 200);
-getRandomDiferentNumber(commentId, 1, 300);
-getRandomDiferentNumber(avatar, 1, 6);
+const ids = [];
+const ID_MIN = 1;
+const ID_MAX = 25;
+
+const likes = [];
+const LIKES_MIN = 15;
+const LIKES_MAX = 200;
+
+const commentIds = [];
+const COMMENT_ID_MIN = 15;
+const COMMENT_ID_MAX = 200;
+
+const avatars = [];
+const AVATAR_MIN = 15;
+const AVATAR_MAX = 200;
+
+const generateComments = () => {
+  const comments = [];
+  const count = getRandomPositiveInteger(1, 3);
+  // const commentId = getRandomDiferentNumber(commentIds , COMMENT_ID_MIN, COMMENT_ID_MAX);
+  // const avatar = getRandomDiferentNumber(avatars, AVATAR_MIN, AVATAR_MAX);
+
+  for (let index = 0; index < count; index++) {
+    const comment = {
+      id: getRandomDiferentNumber(commentIds , COMMENT_ID_MIN, COMMENT_ID_MAX),
+      avatar: `img/avatar-${getRandomDiferentNumber(avatars, AVATAR_MIN, AVATAR_MAX)}.svg`,
+      message:  getRandomArrayElement(message),
+      name: getRandomArrayElement(name),
+    };
+    comments.push(comment);
+  }
+  return comments;
+};
 
 
-const createPost = () => ({
-  id: id,
-  url: `photos/${  url  }.jpg`,
-  description: getRandomArrayElement(description),
-  likes: likes,
-  comments: {
-    id: commentId,
-    avatar: `img/avatar-${  avatar }.svg`,
-    message:  getRandomArrayElement(message),
-    name: getRandomArrayElement(name),
-  },
-});
+function createPost() {
+  const id = getRandomDiferentNumber(ids, ID_MIN, ID_MAX);
+  const like = getRandomDiferentNumber(likes, LIKES_MIN, LIKES_MAX);
 
-let similarPosts = Array.from({ length: 4 }, createPost);
-console.log(similarPosts);
+
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: getRandomArrayElement(description),
+    likes: like,
+    comments: generateComments(),
+  };
+}
+
+const similarPosts = Array.from({ length: 25 }, createPost);
+similarPosts();
